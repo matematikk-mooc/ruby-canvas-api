6932a66414f579aa825cafbbac5f8ccf0356c574
Rom for Læring <Rom for Lring>
2017-03-13 09:36:14 +0100

    Change beta.matematikk.mooc.no to beta-matematikk.mooc.no

--- connection.rb
+++ connection.rb
@@ -1,4 +1,4 @@
-#Forfatter: Erlend Thune (erlend.thune@iktsenteret.no)
+﻿#Forfatter: Erlend Thune (erlend.thune@iktsenteret.no)
 #Dato: Høsten 2015
 #Skrevet i forbindelse med prosjektet matematikkMOOC
 #https://github.com/whitmer/canvas-api
@@ -6,7 +6,7 @@ require 'canvas-api'
 
 #Erstatt prod og beta variablene med dine servere
 $prod = "https://matematikk.mooc.no"
-$beta = "https://beta.matematikk.mooc.no"
+$beta = "https://beta-matematikk.mooc.no"
 def getCanvasConnection(dst)
 	conn = nil;
 	if(dst == "prod")
@@ -14,7 +14,7 @@ def getCanvasConnection(dst)
 		#https://guides.instructure.com/m/4214/l/40399-how-do-i-obtain-an-api-access-token-for-an-account
 		conn = Canvas::API.new(:host => $prod, :token => "Vey7Yc0YExJ1lzFbFNyeAcaU5pstxFbibyAl6ir5")
 	else
-		conn = Canvas::API.new(:host => $beta, :token => "IcucDUlSU6JRlYJr0endPns6AdXkRDGnTzEXt3dy")
+		conn = Canvas::API.new(:host => $beta, :token => "oL61YvmG79Nne6GJZyn7PLaUOuksd2UaXhWx1hjJ")
 	end
 	return conn;
 end
