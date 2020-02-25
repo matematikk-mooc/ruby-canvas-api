8924f4ca00ae5e99896b21718b24eb4604e8cbc3
etsikt <erlend.thune@udir.no>
2020-02-25 23:10:54 +0100

    Add connection example

---
+++ connection.example.rb
@@ -0,0 +1,25 @@
+﻿#Forfatter: Erlend Thune (erlend.thune@iktsenteret.no)
+#Dato: Høsten 2015
+#Skrevet i forbindelse med prosjektet matematikkMOOC
+#https://github.com/whitmer/canvas-api
+require 'canvas-api'
+
+#Erstatt prod og beta variablene med dine servere
+$prod = "https://bibsys.instructure.com"
+$beta = "https://beta-matematikk.mooc.no"
+def getCanvasConnection(dst)
+	conn = nil;
+	if(dst == "prod")
+		#Du må generere ditt eget token. Les hvordan her:
+		#https://guides.instructure.com/m/4214/l/40399-how-do-i-obtain-an-api-access-token-for-an-account
+		$token = ""
+		$host = $prod
+	else
+	    $token = ""
+		$host = $beta
+	end
+	puts $host
+   	conn = Canvas::API.new(:host => $host, :token => $token)
+	return conn;
+end
+
